ab7f58a6e55b6f26c84d08bbf17288560a6d7b95
MCatalin420 <maior.catalin420@gmail.com>
2025-01-09 21:06:41 +0200

    Commit project files

---
+++ app/page.tsx
@@ -0,0 +1,46 @@
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-xl text-center justify-center">
+        <span className={title()}>Gaseste&nbsp;</span>
+        <span className={title({ color: "violet" })}>parcari&nbsp;</span>
+        <br />
+        <span className={title()}>
+         in cel mai scurt timp!
+        </span>
+        <div className={subtitle({ class: "mt-4" })}>
+          O platforma rapida, simpla si intuitiva pentru gasirea locurilor de parcare.
+        </div>
+      </div>
+
+      <div className="flex gap-3">
+        
+        <Link
+          isExternal
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.links.github}
+        >
+          <GithubIcon size={20} />
+          GitHub
+        </Link>
+      </div>
+
+      <div className="mt-8">
+        <Snippet hideCopyButton hideSymbol variant="bordered">
+          <span>
+            Get started by editing <Code color="primary">app/page.tsx</Code>
+          </span>
+        </Snippet>
+      </div>
+    </section>
+  );
+}
